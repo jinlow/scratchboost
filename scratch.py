7acf12ef7db8924481885073a63270c25d749a5f
jinlow <james.d.inlow@gmail.com>
2023-06-17 16:25:57 -0500

    Update scratch file

--- scratch.py
+++ scratch.py
@@ -8,28 +8,27 @@ X = df.select_dtypes("number").fillna(0).drop(columns="survived").to_numpy()
 y = df["survived"]
 
 booster = Booster(
-        iterations=30,
-        learning_rate=0.3,
-        max_depth=5,
-        l2=1,
-        min_leaf_weight=1,
-        gamma=0,
-    )
+    iterations=30,
+    learning_rate=0.3,
+    max_depth=5,
+    l2=1,
+    min_leaf_weight=1,
+    gamma=0,
+)
 booster.fit(X, y)
 bpreds = booster.predict(X)
 
 xgb_booster = xgb.XGBClassifier(
-n_estimators=30, 
-learning_rate=0.3,
-max_depth=5,
-reg_lambda=1,
-min_child_weight=1,
-gamma=0,
-objective="binary:logitraw",
-tree_method="hist",
-nbins=500,
+    n_estimators=30,
+    learning_rate=0.3,
+    max_depth=5,
+    reg_lambda=1,
+    min_child_weight=1,
+    gamma=0,
+    objective="binary:logitraw",
+    tree_method="hist",
+    eval_metric="auc",
 )
 xgb_booster.fit(X, y)
 xpreds = xgb_booster.predict(X, output_margin=True)
 np.allclose(bpreds, xpreds)
-
